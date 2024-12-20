ef7728c080138cbea3b6048b1d01eabf10bde70d
Srinivas Bharadwaj K <133909958+Srinivaseena1018@users.noreply.github.com>
2024-12-20 17:40:49 +0530

    Add files via upload

---
+++ MUTUAL FRIEND RECOMMENDATION IN MSNs/Service_Provider/views.py
@@ -0,0 +1,222 @@
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
+from django.db.models import Count
+from django.db.models import Q
+import datetime
+import xlwt
+from django.http import HttpResponse
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,predict_friendship_inference,detection_ratio,detection_accuracy
+
+
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
+        password = request.POST.get('password')
+        if admin == "Admin" and password =="Admin":
+            detection_accuracy.objects.all().delete()
+            return redirect('View_Remote_Users')
+
+    return render(request,'SProvider/serviceproviderlogin.html')
+
+def View_Predicted_Friendship_Inference_Matched_Status_Ratio(request):
+    detection_ratio.objects.all().delete()
+    ratio = ""
+    kword = 'No Match Found'
+    print(kword)
+    obj = predict_friendship_inference.objects.all().filter(Q(Prediction=kword))
+    obj1 = predict_friendship_inference.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
+
+    ratio12 = ""
+    kword12 = 'Match Found'
+    print(kword12)
+    obj12 = predict_friendship_inference.objects.all().filter(Q(Prediction=kword12))
+    obj112 = predict_friendship_inference.objects.all()
+    count12 = obj12.count();
+    count112 = obj112.count();
+    ratio12 = (count12 / count112) * 100
+    if ratio12 != 0:
+        detection_ratio.objects.create(names=kword12, ratio=ratio12)
+
+
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Predicted_Friendship_Inference_Matched_Status_Ratio.html', {'objs': obj})
+
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
+
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
+
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
+
+def View_Predicted_Friendship_Inference_Matched_Status(request):
+    obj =predict_friendship_inference.objects.all()
+    return render(request, 'SProvider/View_Predicted_Friendship_Inference_Matched_Status.html', {'list_objects': obj})
+
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
+
+
+def Download_Predicted_DataSets(request):
+
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Datasets.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = predict_friendship_inference.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+        row_num = row_num + 1
+
+        ws.write(row_num, 0, my_row.Fid, font_style)
+        ws.write(row_num, 1, my_row.Name, font_style)
+        ws.write(row_num, 2, my_row.Gender, font_style)
+        ws.write(row_num, 3, my_row.DOB, font_style)
+        ws.write(row_num, 4, my_row.Interests, font_style)
+        ws.write(row_num, 5, my_row.City, font_style)
+        ws.write(row_num, 6, my_row.Country, font_style)
+        ws.write(row_num, 7, my_row.FName, font_style)
+        ws.write(row_num, 8, my_row.FGender, font_style)
+        ws.write(row_num, 9, my_row.FDOB, font_style)
+        ws.write(row_num, 10, my_row.FCity, font_style)
+        ws.write(row_num, 11, my_row.FCountry, font_style)
+        ws.write(row_num, 12, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def train_model(request):
+    detection_accuracy.objects.all().delete()
+
+    df = pd.read_csv('Datasets.csv')
+
+    def apply_response(Label):
+        if (Label == 0):
+            return 0  # No Match Found
+        elif (Label == 1):
+            return 1  # Match Found
+
+    df['results'] = df['Label'].apply(apply_response)
+
+    X = df['Fid']
+    y = df['results']
+
+    print("UserID")
+    print(X)
+    print("Results")
+    print(y)
+
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+    print("Convolutional Neural network--CNN")
+    from sklearn.neural_network import MLPClassifier
+    mlpc = MLPClassifier().fit(X_train, y_train)
+    y_pred = mlpc.predict(X_test)
+    testscore_mlpc = accuracy_score(y_test, y_pred)
+    accuracy_score(y_test, y_pred)
+    print(accuracy_score(y_test, y_pred))
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('MLPClassifier', mlpc))
+    detection_accuracy.objects.create(names="Convolutional Neural network--CNN", ratio=accuracy_score(y_test, y_pred) * 100)
+
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    models.append(('svm', lin_clf))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+    print("Logistic Regression")
+
+    from sklearn.linear_model import LogisticRegression
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('logistic', reg))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+    print("Decision Tree Classifier")
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    dtcpredict = dtc.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, dtcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, dtcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, dtcpredict))
+    models.append(('DecisionTreeClassifier', dtc))
+    detection_accuracy.objects.create(names="Decision Tree Classifier", ratio=accuracy_score(y_test, dtcpredict) * 100)
+
+    print("SGD Classifier")
+    from sklearn.linear_model import SGDClassifier
+    sgd_clf = SGDClassifier(loss='hinge', penalty='l2', random_state=0)
+    sgd_clf.fit(X_train, y_train)
+    sgdpredict = sgd_clf.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, sgdpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, sgdpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, sgdpredict))
+    models.append(('SGDClassifier', sgd_clf))
+    detection_accuracy.objects.create(names="SGD Classifier", ratio=accuracy_score(y_test, sgdpredict) * 100)
+
+    csv_format = 'Results.csv'
+    df.to_csv(csv_format, index=False)
+    df.to_markdown
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/train_model.html', {'objs': obj})
